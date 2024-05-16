fce7874bee5baa181bd762693d400db6d39e4731
Ktaf <kouroshtaf@gmail.com>
2024-05-16 13:53:50 +0300

    yubikey: Uncomment pkgs

--- modules/yubikey.nix
+++ modules/yubikey.nix
@@ -8,15 +8,16 @@
       pcsctools
       yubikey-personalization
       yubikey-personalization-gui
-      # yubikey-manager
-      # yubikey-manager-qt
+      yubikey-manager
+      yubikey-manager-qt
       yubico-piv-tool
-      # yubioath-flutter
+      yubioath-flutter
     ];
     shellInit = ''
       gpg-connect-agent /bye
       export GPG_TTY="$(tty)"
       export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+      gpgconf --launch gpg-agent
     '';
   };
   programs = {
@@ -26,6 +27,7 @@
       enableSSHSupport = true;
       pinentryPackage = pkgs.pinentry-gnome3;
       settings = {
+        ttyname = "$GPG_TTY";
         default-cache-ttl = 900;
         max-cache-ttl = 999999;
       };
