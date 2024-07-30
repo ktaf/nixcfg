9dc7922652ddedc57bbdd6801e11070f92557738
ktaf <kouroshtaf@gmail.com>
2024-07-30 14:07:00 +0300

    chore: Enable IPv6 support

--- hosts/arvanix/network.nix
+++ hosts/arvanix/network.nix
@@ -4,6 +4,7 @@
   networking = {
     hostName = "arvanix";
     nameservers = [ "45.90.28.154" "45.90.30.154" "2a07:a8c0::18:68b2" "2a07:a8c1::18:68b2" ];
+    enableIPv6 = true;
 
     # Enable IP forwarding
     firewall.enable = false;
@@ -17,7 +18,7 @@
         remote = "90.190.123.24";  # Replace with your Mikrotik's domain
         local = "185.226.118.126";      # Replace with your NixOS server's domain
         ttl = 255;
-        dev = "sit1";  # Specify a device name
+        dev = "enp3s0";  # Specify a device name
       };
     };
   };
