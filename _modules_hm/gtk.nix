904596f0f60485f0c29b7272abd2ec4f3cc0abe5
ktaf <kouroshtaf@gmail.com>
2024-10-19 22:19:33 +0300

    chore: Added GTK module

---
+++ _modules_hm/gtk.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:{
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    font = {
+      name = "Hack Nerd 10";
+      package = pkgs.hack-font;
+    };
+  };
+}
