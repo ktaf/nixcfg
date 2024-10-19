94b1428cf95be72f9a790d45e671afe4b20e6f64
ktaf <kouroshtaf@gmail.com>
2024-10-19 22:19:18 +0300

    chore: Added rofi module

---
+++ _modules_hm/rofi.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:{
+
+  programs.rofi = {
+    enable = true;
+    # configPath = " ../.config/rofi/config.rasi";
+    theme = builtins.readFile ../.config/rofi/themes/catppuccin-mocha.rasi;
+  };
+}
