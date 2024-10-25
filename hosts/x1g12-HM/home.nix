d1a5466bbeccc4739251df09abb7c4073014164a
ktaf <kourosh.tafreshi@bolt.eu>
2024-10-25 15:06:22 +0300

    chore: Merged nixGL under use PR to Home-Manager

--- hosts/x1g12-HM/home.nix
+++ hosts/x1g12-HM/home.nix
@@ -3,18 +3,6 @@ let
   user = "kourosh";
 in
 {
-  # TODO: remove when https://github.com/nix-community/home-manager/pull/5355 gets merged:
-  imports = [
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
-      sha256 = "1krclaga358k3swz2n5wbni1b2r7mcxdzr6d7im6b66w3sbpvnb3";
-    })
-  ];
-  # {
-  #   systemd.user = {
-  #     targets.sway-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
-  #   };
-
   nixGL = {
     packages = nixGL.packages;
   };
