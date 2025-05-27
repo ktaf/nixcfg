9a251981f4e1f076efb8e89b31f07f15846d8295
ktaf <kourosh.tafreshi@bolt.eu>
2025-05-27 15:51:15 +0300

    chore: Introduce a new monitor to the mix

--- _modules_hm/gui/kanshi.nix
+++ _modules_hm/gui/kanshi.nix
@@ -9,14 +9,18 @@
           outputs = [
             {
               criteria = "eDP-1";
-              mode = "1920x1200@60Hz";
-              position = "3440,240";
+              status = "disable";
             }
             {
-              criteria = "Dell Inc. DELL S3422DWG FSF4KK3";
-              mode = "3440x1440@99.98Hz";
+              criteria = "Samsung Electric Company Odyssey G93SC HNTX201462";
+              mode = "5120x1440@240.000Hz";
               position = "0,0";
             }
+            # {
+            #   criteria = "Dell Inc. DELL S3422DWG FSF4KK3";
+            #   mode = "3440x1440@99.98Hz";
+            #   position = "0,0";
+            # }
           ];
           # exec = [
           #   "[ $(cat /proc/acpi/button/lid/LID0/state | cut -f2 -d':' | tr -d '[:space:]') = closed ] && swaymsg output eDP-1 disable || swaymsg output eDP-1 enable"
