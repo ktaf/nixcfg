99d34a7fb9b8ac1f13a3cdf3bc3a14773d18635a
ktaf <kourosh.tafreshi@bolt.eu>
2025-08-09 12:28:30 +0300

    upgrade: GTX1050->P106-100

--- hosts/ed800/vms.nix
+++ hosts/ed800/vms.nix
@@ -5,7 +5,7 @@
     enable = true;
     qemu = {
       package = pkgs.qemu_kvm;
-      runAsRoot = false;
+      runAsRoot = true;
       swtpm.enable = true;
       ovmf.enable = true;
       ovmf.packages = [ pkgs.OVMFFull.fd ];
@@ -18,7 +18,7 @@
       "intel_iommu=on"
       "iommu=pt"
       "nvidia.NVreg_UsePageAttributeTable=1"
-      "nvidia.NVreg_EnablePCIeGen3=1"
+      # "nvidia.NVreg_EnablePCIeGen3=1"
       "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
     ];
     kernelModules = [
@@ -28,9 +28,8 @@
       "vfio_virqfd"
     ];
     # options vfio-pci ids=10de:1c82,10de:0fb9  #GTX 1050
-    # options vfio-pci ids=10de:1c07 #P106-100
     extraModprobeConfig = ''
-      options vfio-pci ids=10de:1c02,10de:10f1
+      options vfio-pci ids=10de:1c07 #P106-100
     '';
   };
 
