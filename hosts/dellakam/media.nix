f52a965f6c746090741365d052c9e136751e6b80
ktaf <kourosh.tafreshi@bolt.eu>
2026-04-25 21:39:03 +0300

    chore: dismantle br0

--- hosts/dellakam/media.nix
+++ hosts/dellakam/media.nix
@@ -21,54 +21,10 @@ in
       enable = true;
       openFirewall = true;
     };
-
-    samba-wsdd = {
-      enable = true;
-      openFirewall = true;
-      workgroup = "WORKGROUP";
-      hostname = "dellakam";
-    };
-
-    samba = {
-      enable = true;
-      package = pkgs.samba4Full;
-      openFirewall = true;
-      nsswins = true;
-      nmbd.enable = true;
-      settings = {
-        global = {
-          workgroup = "WORKGROUP";
-          "server string" = "dellakam";
-          "map to guest" = "Never";
-          "server min protocol" = "SMB3";
-          security = "user";
-          "netbios name" = "dellakam";
-          "os level" = "65";
-          "socket options" = "TCP_NODELAY IPTOS_LOWDELAY SO_RCVBUF=262144 SO_SNDBUF=262144";
-          "use sendfile" = "yes";
-          "aio read size" = "16384";
-          "aio write size" = "16384";
-          "max xmit" = "131072";
-          "kernel oplocks" = "no";
-          "level2 oplocks" = "no";
-        };
-        public = {
-          path = "/data/samba/public";
-          browseable = "yes";
-          "read only" = "no";
-          "guest ok" = "no";
-          "force user" = "win";
-          "force group" = "win";
-          "valid users" = "win";
-          "create mask" = "0664";
-          "directory mask" = "0775";
-          "force directory mode" = "2775";
-        };
-      };
-    };
-
     overseerr.enable = true;
+
     sabnzbd.enable = true;
+
     sonarr.enable = true;
     radarr.enable = true;
     bazarr.enable = true;
@@ -132,6 +88,51 @@ in
     "d /data/downloads/watch 2775 transmission win -"
     "z /data/downloads/watch 2775 transmission win -"
   ];
+
+  samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+    workgroup = "WORKGROUP";
+    hostname = "dellakam";
+  };
+
+  samba = {
+    enable = true;
+    package = pkgs.samba4Full;
+    openFirewall = true;
+    nsswins = true;
+    nmbd.enable = true;
+    settings = {
+      global = {
+        workgroup = "WORKGROUP";
+        "server string" = "dellakam";
+        "map to guest" = "Never";
+        "server min protocol" = "SMB3";
+        security = "user";
+        "netbios name" = "dellakam";
+        "os level" = "65";
+        "socket options" = "TCP_NODELAY IPTOS_LOWDELAY SO_RCVBUF=262144 SO_SNDBUF=262144";
+        "use sendfile" = "yes";
+        "aio read size" = "16384";
+        "aio write size" = "16384";
+        "max xmit" = "131072";
+        "kernel oplocks" = "no";
+        "level2 oplocks" = "no";
+      };
+      public = {
+        path = "/data/samba/public";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "force user" = "win";
+        "force group" = "win";
+        "valid users" = "win";
+        "create mask" = "0664";
+        "directory mask" = "0775";
+        "force directory mode" = "2775";
+      };
+    };
+  };
 
   users.groups.win = { };
   users.users.win = {
--- hosts/dellakam/network.nix
+++ hosts/dellakam/network.nix
@@ -9,13 +9,6 @@
     # bridges.br0.interfaces = [ "enp3s0" ];
     # interfaces.br0.useDHCP = true;
 
-    interfaces.enp3s0.ipv4.addresses = [
-      {
-        address = "192.168.2.100";
-        prefixLength = 24;
-      }
-    ];
-
     # Enable networking
     networkmanager.enable = true;
 
